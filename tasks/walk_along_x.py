17eba0b650ab1b71936240d52787523c0307a04a
Roshin Rajan Panackal <roshin.panackal@gmail.com>
2022-07-24 02:28:33 +0200

    linear distance rew added

--- tasks/walk_along_x.py
+++ tasks/walk_along_x.py
@@ -4,20 +4,20 @@ class WalkAlongX(object):
     """Task to walk along a straight line (x-axis)"""
     def __init__(self,
                 #forward_reward_cap: float = float("inf"),
-                velocity_weight: float = 0.0001,
-                #distance_weight: float = 1.0,
+                velocity_weight: float = 0.00005,
+                distance_weight: float = 1.0,
                 step_weight : float = 1,
                 # energy_weight=0.0005,
-                #shake_weight: float = 0.005,
+                shake_weight: float = 0.005,
                 drift_weight: float = 100,
                 #action_cost_weight: float = 0.02,
                 # deviation_weight: float = 1,
                 enable_roll_limit : bool = True,
-                healthy_roll_limit : float = np.pi * 3/4,
+                healthy_roll_limit : float = np.pi * 1/2,
                 # roll_threshold: float = np.pi * 1/2,
                 # pitch_threshold: float = 0.8,
                 enable_z_limit: bool = True,
-                healthy_z_limit: float = 0.13,
+                healthy_z_limit: float = 0.15,
                 # healthy_reward=1.0,
                 ):
         """Initializes the task."""
@@ -25,7 +25,7 @@ class WalkAlongX(object):
         #self._forward_reward_cap = forward_reward_cap
         #self._action_cost_weight = action_cost_weight
         self._velocity_weight = velocity_weight
-        #self._distance_weight = distance_weight
+        self._distance_weight = distance_weight
         #self._shake_weight = shake_weight
         self._drift_weight = drift_weight
         self._step_weight = step_weight
@@ -39,6 +39,8 @@ class WalkAlongX(object):
         self.healthy_roll_limit = healthy_roll_limit
 
         self.step_counter = 0
+
+        self._target_pos =  50
 
     def __call__(self, env):
         return self.reward(env)
@@ -61,6 +63,7 @@ class WalkAlongX(object):
         #print(f"Initial Orienttaion {self._init_base_ori_euler}")
         self.step_counter = 0
 
+        
     def update(self, env):
         """Updates the internal state of the task.
         Evoked after call to a1.A1.Step(), ie after action takes effect in simulation
@@ -98,8 +101,13 @@ class WalkAlongX(object):
         # action_reward = -self._action_cost_weight * np.linalg.norm(self._last_action) / 12
         drift_reward =  - self._drift_weight * (self._current_base_pos[1])  ** 2
         
+        distance_reward = - self._distance_weight * (self._target_pos - self._current_base_pos[0])
+        # orientation = env.robot.GetBaseOrientation()
+        # rot_matrix = env.robot._pybullet_client.getMatrixFromQuaternion(orientation)
+        # local_up_vec = rot_matrix[6:]
+        # shake_reward = -abs(np.dot(np.asarray([1, 1, 0]), np.asarray(local_up_vec)))
         #orientation_reward = -sum(abs(self._current_base_ori_euler - self._init_base_ori_euler))
-        reward = x_velocity_reward + drift_reward + self.step_counter # + y_velocity_reward + forward_reward + displacement_reward + action_reward \
+        reward = x_velocity_reward + drift_reward + self.step_counter + distance_reward #+ shake_reward # + y_velocity_reward + forward_reward + displacement_reward + action_reward \
                   #+ orientation_reward
 
         #print("Reward", reward)
