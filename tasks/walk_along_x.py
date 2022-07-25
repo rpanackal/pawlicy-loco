9da5eee273bf220cf5553ad690179ec76cf82412
Roshin Rajan Panackal <roshin.panackal@gmail.com>
2022-07-25 09:00:58 +0200

    Reward - velocity based

--- tasks/walk_along_x.py
+++ tasks/walk_along_x.py
@@ -4,21 +4,21 @@ class WalkAlongX(object):
     """Task to walk along a straight line (x-axis)"""
     def __init__(self,
                 #forward_reward_cap: float = float("inf"),
-                velocity_weight: float = 0.005,
+                velocity_weight: float = 1,
                 distance_weight: float = 0.1,
                 step_weight : float = 1,
                 # energy_weight=0.0005,
                 shake_weight: float = 0.005,
-                drift_weight: float = 100,
+                drift_weight: float = 1,
                 orientation_weight : float = 1,
                 #action_cost_weight: float = 0.02,
                 # deviation_weight: float = 1,
                 enable_roll_limit : bool = True,
-                healthy_roll_limit : float = np.pi * 1/2,
+                healthy_roll_limit : float = np.pi * 3/4,
                 # roll_threshold: float = np.pi * 1/2,
                 # pitch_threshold: float = 0.8,
                 enable_z_limit: bool = True,
-                healthy_z_limit: float = 0.13,
+                healthy_z_limit: float = 0.15,
                 # healthy_reward=1.0,
                 ):
         """Initializes the task."""
@@ -88,30 +88,31 @@ class WalkAlongX(object):
             If the robot base becomes unstable (based on orientation), the episode
             terminates early.
         """
+        
         return not self.is_healthy
 
 
     def reward(self, env):
 
         # bug : -ve velocity along y is rewarded
-        velocity_reward = np.dot([1, -1, 0], self._current_base_vel)
-        # x_velocity_reward = self._velocity_weight * self._current_base_vel[0]
-        forward_reward = self._distance_weight * self._current_base_pos[0] - self._init_base_pos[0]
-        # displacement_reward = self._current_base_pos[0] - self._last_base_pos[0]
+        #velocity_reward = np.dot([1, -1, 0], self._current_base_vel)
+        velocity_reward = self._velocity_weight * self._current_base_vel[0]
+        # # forward_reward = self._distance_weight * (self._current_base_pos[0] - self._init_base_pos[0])
+        # #displacement_reward = self._current_base_pos[0] - self._last_base_pos[0]
 
-        # y_velocity_reward = -abs(self._current_base_vel[1])
-        # action_reward = -self._action_cost_weight * np.linalg.norm(self._last_action) / 12
+        # # y_velocity_reward = -abs(self._current_base_vel[1])
+        # # action_reward = -self._action_cost_weight * np.linalg.norm(self._last_action) / 12
         
         
        
-        # orientation = env.robot.GetBaseOrientation()
-        # rot_matrix = env.robot._pybullet_client.getMatrixFromQuaternion(orientation)
-        # local_up_vec = rot_matrix[6:]
-        # shake_reward = -abs(np.dot(np.asarray([1, 1, 0]), np.asarray(local_up_vec)))
-        drift_reward =  - self._drift_weight * (self._current_base_pos[1])  ** 2
-        #distance_reward = - self._distance_weight * np.linalg.norm(self._target_pos - self._current_base_pos)
-        orientation_reward = - self._orientation_weight * np.linalg.norm(env.robot.GetTrueBaseRollPitchYaw() - self._init_base_ori_euler)
-        reward = self._step_weight + drift_reward + forward_reward + orientation_reward + velocity_reward
+        # # orientation = env.robot.GetBaseOrientation()
+        # # rot_matrix = env.robot._pybullet_client.getMatrixFromQuaternion(orientation)
+        # # local_up_vec = rot_matrix[6:]
+        # # shake_reward = -abs(np.dot(np.asarray([1, 1, 0]), np.asarray(local_up_vec)))
+        # drift_reward =  - self._drift_weight * (self._current_base_pos[1])  ** 2
+        # #distance_reward = - self._distance_weight * np.linalg.norm(self._target_pos - self._current_base_pos)
+        # orientation_reward = - self._orientation_weight * np.linalg.norm(env.robot.GetTrueBaseRollPitchYaw() - self._init_base_ori_euler)
+        reward = velocity_reward
             # x_velocity_reward + drift_reward + self.step_counter + distance_reward + orientation_reward
             #+ shake_reward # + y_velocity_reward + forward_reward + displacement_reward + action_reward \
                   #
@@ -130,40 +131,18 @@ class WalkAlongX(object):
         # Isuue - needs to account for heightfield data
         if self.enable_z_limit and (self._current_base_pos[2] < self.healthy_z_limit):
             return False
-        # if self.enable_roll_limit and (self._current_base_ori_euler[0] < -self.healthy_roll_limit or \
-        #     self._current_base_ori_euler[0] > self.healthy_roll_limit):
-        #     return False
 
-        # Checking if robot is in contact with the ground
-        # foot_links = env.robot.GetFootLinkIDs()
-        # ground = env.get_ground()
-        # # Skip the first env step
-        # if env.env_step_counter > 0:
-        #     robot_ground_contacts = env.pybullet_client.getContactPoints(bodyA=env.robot.quadruped, bodyB=ground["id"])
-        #     for contact in robot_ground_contacts:
-        #         # Only the toes of the robot should in contact with the ground
-        #         if contact[3] not in foot_links:
-        #             # print("contact_fail")
-        #             return False
-
-            # # The robot shouldn't be flipped, so limit the Roll and Pitch
-            # if self._current_base_ori_euler[0] > self.roll_threshold or self._current_base_ori_euler[0] < -self.roll_threshold:
-            #     # print("roll_fail")
-            #     return False
-            # if self._current_base_ori_euler[1] > self.pitch_threshold or self._current_base_ori_euler[1] < -self.pitch_threshold:
-            #     # print("pitch_fail")
-            #     return False
-
-            # Issue - needs to account for heightfield data
-            # if self.enable_z_limit:
-            #     # z_upper_limit = self._init_base_pos[2] + self.healthy_z_limit * 2
-            #     # z_lower_limit = self._init_base_pos[2] - self.healthy_z_limit
-            #     # # Random terrains don't have fixed heights in every run. Hence initial position of
-            #     # # the robot is generally higher than usual. Taking that under consideration
-            #     # if env.world_dict["ground"]["type"] != "plain":
-            #     #     z_lower_limit -= 0.1
-            #     # Robot shouldn't be below the limit
-            #     if self._current_base_pos[2] < 0.02:
-            #         # print("z_fail: Current=", self._current_base_pos[2], ",\tLimit=", z_lower_limit)
-            #         return False
         return True
+
+    def is_fallen(self, env):
+        """Decide whether Rex has fallen.
+        If the up directions between the base and the world is larger (the dot
+        product is smaller than 0.85) or the base is very low on the ground
+        (the height is smaller than 0.13 meter), rex is considered fallen.
+        Returns:
+          Boolean value that indicates whether rex has fallen.
+        """
+        orientation = env.robot.GetBaseOrientation()
+        rot_mat = env.robot._pybullet_client.getMatrixFromQuaternion(orientation)
+        local_up = rot_mat[6:]
+        return np.dot(np.asarray([0, 0, 1]), np.asarray(local_up)) < 0.85
